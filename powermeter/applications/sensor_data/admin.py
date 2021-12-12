c7f3b57df8692a23465c34b7004f4109e9ba2098
Emmanuel Torres Molina <eotorresmolina@gmail.com>
2021-12-12 15:26:12 -0300

    Regitered Measurement Model in Django Admin

--- powermeter/applications/sensor_data/admin.py
+++ powermeter/applications/sensor_data/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+# Import my models:
+from applications.sensor_data.models import *
+
 # Register your models here.
+@admin.register(Measurement)
+class MeasurementAdmin(admin.ModelAdmin):
+    '''
+    Clase para registrar mi modelo 'Measurement' para
+    que pueda ser utilizado desde el admin de Django.
+    '''
+    pass
+
