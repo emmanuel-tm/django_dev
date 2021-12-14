151d8f37b0495fe086dad55864ed28427b08e839
Emmanuel Torres Molina <eotorresmolina@gmail.com>
2021-12-14 00:44:33 -0300

    Configured Measurement Model in Django Admin.

--- powermeter/applications/sensor_data/admin.py
+++ powermeter/applications/sensor_data/admin.py
@@ -10,5 +10,23 @@ class MeasurementAdmin(admin.ModelAdmin):
     Clase para registrar mi modelo 'Measurement' para
     que pueda ser utilizado desde el admin de Django.
     '''
-    pass
+
+    # Filtro por el atributo 'date'.
+    list_filter = ['date']
+
+    # Permito que se realicen búsqueda por date(fechas).
+    search_fields = ['date']
+
+    # Campos que deseo mostrar y aquellos que quiero que
+    # estén ocultos, pero se puedan acceder mediante
+    # el botón "Opciones Avanzadas".
+    fieldsets = (
+        (None, {
+            'fields': ('value', 'date')
+        }),
+        ('Advanced options', {
+            'classes': ('collapse',),
+            'fields': ('time',),
+        }),
+    )
 
